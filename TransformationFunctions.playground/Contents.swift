7df5aadd9bf1bb0aeab2d0943898ff16e1131555
GlebProkofev <prokofyevg@yandex.ru>
2025-01-30 15:55:43 +0300

    тренировка

--- TransformationFunctions.playground/Contents.swift
+++ TransformationFunctions.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-// Функции высшего порядка
+// Функции высшего порядка — это функции, которые принимают другие функции в качестве аргументов или возвращают их в качестве результата.
 
 // MARK: - Transformation Functions
 
@@ -28,6 +28,34 @@ let celsiusTemperatures = [0, 20, 37, 100]
 let fahrenheitTemperatures = celsiusTemperatures.map { $0 * 9 / 5 + 32 }
 
 print(fahrenheitTemperatures)
+
+// функция compactMap
+// позволяет обрабатывать коллекции, удаляя значения nil из результата преобразования.
+
+// пример
+let stringNumbers = ["1", "2", "three", "4", "5"]
+let validNumbers = stringNumbers.compactMap { Int($0) }
+print(validNumbers) // [1, 2, 4, 5]
+
+// у вас есть массив словарей, представляющих пользователей. Каждый словарь содержит имя и возраст пользователя. Ваша задача — создать новый массив, который будет содержать только имена пользователей, чей возраст является допустимым целым числом (например, больше 0) и преобразовать возраст в целое число.
+
+let users: [[String: String]] = [
+    ["name": "Alice", "age": "30"],
+    ["name": "Bob", "age": "invalid"],
+    ["name": "Charlie", "age": "-5"],
+    ["name": "David", "age": "25"],
+    ["name": "Eve", "age": "twenty"]
+]
+
+let validUserNames = users.compactMap { user -> String? in
+    if let ageString = user["age"], let age = Int(ageString), age > 0 {
+        return user["name"]
+    }
+    return nil
+}
+
+print("Пользователи с допустимым возрастом: \(validUserNames)")
+
 
 
 
